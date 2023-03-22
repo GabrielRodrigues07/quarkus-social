ecf2d223dda1097b9590f3881b186a4950b0e6a7
GabrielRodrigues07 <grodrigues256@gmail.com>
2023-03-22 11:27:00 -0300

    adicionando docker-compose, conectando banco de dados e criando classe UserResource

---
+++ src/main/java/io/github/gabriel/quarkussocial/rest/UserResource.java
@@ -0,0 +1,23 @@
+package io.github.gabriel.quarkussocial.rest;
+
+import io.github.gabriel.quarkussocial.rest.dto.CreateUserRequest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserResource {
+
+    @POST
+    public Response createUser(CreateUserRequest userRequest) {
+        return Response.ok(userRequest).build();
+    }
+
+    @GET
+    public Response listAllUsers() {
+        return Response.ok().build();
+    }
+}
---
+++ src/main/resources/db/migration/V0.0.1__CRIAR_TABELA_USER.sql
@@ -0,0 +1,6 @@
+CREATE TABLE USERS
+(
+    id   bigserial    NOT NULL PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    age  integer      NOT NULL
+)
