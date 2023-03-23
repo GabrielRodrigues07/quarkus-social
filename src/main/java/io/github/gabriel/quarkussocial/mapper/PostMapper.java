0ab9cc56feec1f495081ee3693cc81ceeb51fe75
GabrielRodrigues07 <grodrigues256@gmail.com>
2023-03-23 20:36:16 -0300

    Corrigindo Bug

--- src/main/java/io/github/gabriel/quarkussocial/mapper/PostMapper.java
+++ src/main/java/io/github/gabriel/quarkussocial/mapper/PostMapper.java
@@ -5,10 +5,12 @@ import io.github.gabriel.quarkussocial.domain.model.User;
 import io.github.gabriel.quarkussocial.rest.dto.CreatePostRequest;
 import io.github.gabriel.quarkussocial.rest.dto.PostResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "cdi")
 public interface PostMapper {
 
+    @Mapping(target = "id", ignore = true)
     Post toModel(CreatePostRequest postRequest, User user);
 
     PostResponse toResponse(Post post);
