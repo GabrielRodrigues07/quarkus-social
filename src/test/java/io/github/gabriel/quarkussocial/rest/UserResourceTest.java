d58f63853302bfb96a566d687426ee388d39ec3b
GabrielRodrigues07 <grodrigues256@gmail.com>
2023-03-25 11:30:19 -0300

    Testando o caso de erro de validação

--- src/test/java/io/github/gabriel/quarkussocial/rest/UserResourceTest.java
+++ src/test/java/io/github/gabriel/quarkussocial/rest/UserResourceTest.java
@@ -4,27 +4,57 @@ import io.github.gabriel.quarkussocial.rest.dto.CreateUserRequest;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
-@DisplayName("should create and user successfully")
 class UserResourceTest {
 
-    @Test
-    public void createUserTest() {
-        var user = new CreateUserRequest();
+    CreateUserRequest user;
+
+    @BeforeEach
+    public void setUP() {
+        user = new CreateUserRequest();
         user.setName("Fulano");
         user.setAge(30);
-        var response = RestAssured.given().contentType(ContentType.JSON)
+    }
+
+    @Test
+    @DisplayName("should create and user successfully")
+    public void createUserTest() {
+        var response = given().contentType(ContentType.JSON)
                 .body(user)
                 .when()
                 .post("/users")
                 .then().extract().response();
         assertEquals(201, response.getStatusCode());
         assertNotNull(response.jsonPath().getString("id"));
+    }
+
+    @Test
+    @DisplayName("Should return error when json is not valid")
+    public void createUserValidationErrorTest() {
+        user.setName(" ");
+        user.setAge(null);
+        var response = given().contentType(ContentType.JSON)
+                .body(user)
+                .when()
+                .post("/users")
+                .then()
+                .extract().response();
+        assertEquals(422, response.getStatusCode());
+        assertEquals("Validation Error", response.jsonPath().getString("message"));
+
+        List<Map<String, String>> errors = response.jsonPath().getList("errors");
+        assertNotNull(errors.get(0).get("message"));
+        assertNotNull(errors.get(1).get("message"));
     }
 }
